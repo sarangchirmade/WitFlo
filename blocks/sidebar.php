e00ad9d9c0d035411af2e693f78127c901444019
patilbhupesh <37431608+patilbhupesh@users.noreply.github.com>
2018-03-24 07:35:55 +0000

    Add files via upload

---
+++ blocks/sidebar.php
@@ -0,0 +1,56 @@
+<style media="screen">
+
+</style>
+
+<div class="wrapper" >
+
+  <nav id="sidebar"  class="collapse bg-light" >
+
+      <div class="sidebar-header text-center my-2">
+          <h6 class=" d-inline-block"> <strong>Menu</strong> </h6>
+          <button type="button" class="close  d-inline-block mx-2" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+                <span>&times;</span>
+          </button>
+        </div>
+
+      <hr class="mt-0">
+      <!-- <form class="form-inline"> -->
+
+      <div class="d-flex flex-column">
+      <!-- <div class="p-2">Flex item 1</div> -->
+      <div ><li class=" list-unstyled mb-2 mx-2"><button class="btn btn-light btn-circle mx-1" type="submit"><strong>SK</strong></button></li></div>
+      <div class="mx-2"><li class=" list-unstyled mx-2"><button type="button" class="btn btn-sm btn-block  text-left" name="button"><i class="fa fa-user-plus mr-2"></i> <strong>Invite ...</strong></button></div>
+      </div>
+
+      <!-- <ul>
+
+        <li class=" list-unstyled mb-3 mx-2"><button type="button" class="btn btn-block" name="button">Invite ...</button></li>
+      </ul> -->
+      <hr style="margin-left:10px;">
+
+      <div class="d-flex flex-column">
+      <div class="mx-2 my-0"><li class=" list-unstyled mx-2"><button type="button" class="btn btn-sm btn-block btn-sidebar-color text-left" name="button" > <div class="d-inline-block  mr-1" style="height:15px;width:15px;background:#0079BF;border-radius:3px; margin:0;"></div> <div class="d-inline-block" ><strong  >Change Background</strong></div> </button></div>
+      <div class="mx-2"><li class=" list-unstyled mx-2"><button type="button" class="btn btn-sm btn-block btn-sidebar-color text-left" name="button"> <i class="fa fa-filter mr-2"></i> <strong>Filter Cards</strong></button></div>
+      <div class="mx-2"><li class=" list-unstyled mx-2"><button type="button" class="btn btn-sm btn-block btn-sidebar-color text-left" style="padding-bottom:0;"  name="button"><i class="fa fa-rocket mr-2"></i><strong>Power-Ups </strong><p class="pl-4">Calender, Google Drive and More ...</p> </button></div>
+      <div class="mx-2"><li class=" list-unstyled mx-2"><button type="button" class="btn btn-sm btn-block btn-sidebar-color text-left" name="button"><i class="fa fa-filter mr-2"></i> <strong>Stickers</strong></button></div>
+      <div class="mx-2"><li class=" list-unstyled mx-2"><button type="button" class="btn btn-sm btn-block btn-sidebar-color text-left" name="button"><i class="fa fa-ellipsis-h mr-2"></i> <strong>More</strong></button></div>
+
+        </div>
+
+        <hr style="margin-left:10px;">
+        <div class="d-flex flex-column">
+        <!-- <div class="p-2">Flex item 1</div> -->
+        <!-- <div ><li class=" list-unstyled mb-2 mx-2"><button class="btn btn-light btn-circle mx-1" type="submit"><strong>SK</strong></button></li></div> -->
+        <div class="mx-2"><li class=" list-unstyled mx-2"><button type="button" class="btn btn-sm btn-block btn-sidebar-color text-left" name="button"><i class="fa fa-list mr-2"></i> <strong>Activity</strong></button></div>
+
+        </div>
+      <!-- </form> -->
+      <!-- <button class="btn btn-light btn-circle mx-1 " type="submit"><strong>SK</strong></button>
+      <button type="button" class="btn btn-light" name="button">Invite ...</button> -->
+      <!-- <li class="active list-unstyled components "><a href="#">Activity</a></li>
+      <li class=" list-unstyled components"><a href="#">About</a></li>
+      <li class=" list-unstyled components"><a href="#">Pages</a></li>
+      <li class=" list-unstyled components"><a href="#">Portfolio</a></li> -->
+  </nav>
+<!-- </div> -->
+</div>
---
+++ header.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+
+    <title>Trello</title>
+    <script src="vendor/jquery/jquery.slim.min.js"></script>
+    <script src="vendor/popper/umd/popper.min.js"></script>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css" >
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+     <script src="assets/js/main.js" ></script>
+      <link rel="stylesheet" href="style.css">
+       <!-- <link rel="stylesheet" href="assets/css/side.css"> -->
+
+
+
+<style>
+	/*.board-header-btn-divider {
+    float: left;
+    border-left: 1px solid hsla(0,0%,100%,.2);
+    height: 14px;
+    margin: 8px 4px 0 0;
+}*/
+
+
+
+
+
+/*.collapse.show{
+   width: 350px;
+
+}*/
+
+</style>
+
+
+
+  </head>
+  <body>
+
+
+    	<div id="mynav">
+    		<nav class="navbar navbar-expand-lg navbar-light bg-trello-nav-col nopadding">
+  <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+  <form class="form-inline my-2 my-lg-0">
+  	<button class="btn btn-sm btn-trello-btn mx-2 text-white" type="submit">
+<span class="mr-1"><img src="trello-logo/trello-mark-blue.svg" alt="LOGO" height="15" width="15"></span>
+  	<strong>Boards</strong></button>
+      <!-- <input class="form-control mr-sm-2" type="search" aria-label="Search"> -->
+      <div class="inner-addon right-addon">
+    		<i class="fa fa-search icons"></i>
+    		<input type="text" class="form-control form-control-sm btn-trello-btn" />
+		</div>
+
+    </form>
+  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
+    <!-- <span class="navbar-toggler-icon"></span>
+  </button> -->
+
+	<!-- <div>
+
+	</div> -->
+  <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
+  	<img class="ml-auto" src="assets/images/tiger.gif" alt="Smiley face" height="50" width="60">
+
+    <form class="form-inline ml-auto">
+      <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+      <button class="btn btn-sm btn-trello-btn mx-1 " type="submit"><i class="fa fa-plus icons"></i></button>
+      <button class="btn btn-sm btn-trello-btn mx-1 " type="submit"><i class="fa fa-info-circle icons"></i></button>
+      <button class="btn btn-sm btn-trello-btn mx-1 " type="submit"><i class="fa fa-bell icons"></i></button>
+      <button class="btn btn-light btn-circle mx-1 " type="submit" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><strong>SK</strong></button>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+
+    </form>
+
+  <!-- </div> -->
+</nav>
+
+<?php  require_once '/blocks/sidebar.php'; ?>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+  </body>
+</html>
